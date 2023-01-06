5d70250e49ef31cfd749ff027bae91f9baaa3b25
M.Hassoun <96958050+Hassoun00@users.noreply.github.com>
2023-01-06 15:41:41 +0200

    Savvy

---
+++ Pipeline /BuildServerDSCconfig.ps1 2/BuildServerDSCconfig.ps1
@@ -0,0 +1,46 @@
+﻿Configuration BuildServer            
+{            
+    Import-DscResource -ModuleName PSDesiredStateConfiguration, cChoco, xSystemSecurity          
+    Node localhost            
+    {       
+        WindowsFeature NETFramework35
+        {
+        Ensure = "Present"
+        Name = "NET-Framework-Features"
+    }
+       cChocoInstaller installChoco
+      {
+        InstallDir = "C:\Program Files (x86)\choco"
+      }
+      cChocoPackageInstaller installGit
+      {
+         Ensure = 'Present'
+         Name = "git"
+         DependsOn = "[cChocoInstaller]installChoco"
+      }
+
+     cChocoPackageInstaller installJenkins
+      {
+         Ensure = 'Present'
+         Name = "jenkins"
+         DependsOn = "[cChocoInstaller]installChoco"
+      }
+      cChocoPackageInstaller installUnity
+      {
+         Ensure = 'Present'
+         Name = "unity"
+         DependsOn = "[cChocoInstaller]installChoco"
+      }
+      xIEEsc EnableIEEscAdmin
+        {
+            IsEnabled = $False
+            UserRole  = "Administrators"
+        }
+        xIEEsc EnableIEEscUser
+        {
+            IsEnabled = $True
+            UserRole  = "Users"
+        }
+
+    }
+}
